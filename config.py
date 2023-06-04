31f46e63ae1e88c49853c39c9b14a5c86c7faf6b
black <no_email@email.com>
2023-06-04 13:49:38 +0400

    test

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_config() -> Mapping[str, Any]:
     return {
-        "GITLAB_TOKEN": os.environ.get("GITLAB_TOKEN", "TEST__token_for__EXAMPLE"),
-        #"SECRET_KEY_HEX": os.environ.get("SECRET_KEY_HEX"),
+        "GITLAB_TOKEN": os.environ.get("GITLAB_TOKEN", "TEST__token_for__EXAMPLE")
     }
